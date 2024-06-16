3070a577fe6311b5b20184042f2ae0e9c7d1cf64
sudo <example@gmail.com>
2024-06-16 13:50:15 +0300

    django web application and tele_bot

--- mysite/settings.py
+++ mysite/settings.py
@@ -30,6 +30,9 @@ INSTALLED_APPS = [
     'core',
     'billing',
     'customers',
+    'account',
+    'drf_spectacular',
+
 ]
 
 MIDDLEWARE = [
@@ -67,9 +70,13 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": "furni",
+        "USER": "postgres",
+        "PASSWORD": "2160607",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
